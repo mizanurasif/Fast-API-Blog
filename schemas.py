c97a878aae2f1a311a8ac93459e18725b498857c
mizanurasif02 <mizanurasif02@gmail.com>
2026-09-18 02:14:30 +0600

    authorization and authentication

--- schemas.py
+++ schemas.py
@@ -6,19 +6,32 @@ class UserBase(BaseModel):
     email: EmailStr = Field(max_length=120)
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
 
-class UserResponse(UserBase):    
+class UserPublic(BaseModel):    
     model_config= ConfigDict(from_attributes = True)
 
     id: int
+    username: str
     image_file: str | None
     image_path: str
+
+class UserPrivate(UserPublic):    
+    model_config= ConfigDict(from_attributes = True)
+    email: EmailStr = Field(max_length=120)
+
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default= None, min_length=1,max_length=50)
     email: EmailStr | None = Field(default= None, max_length=120)
     image_file: str | None = Field (default= None, min_length=1, max_length=200)
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -26,7 +39,7 @@ class PostBase(BaseModel):
 
 
 class PostCreate(PostBase):
-    user_id: int 
+    pass
 
 class PostUpdate(BaseModel):
     title: str | None = Field(default= None, min_length=1, max_length=100)
@@ -38,4 +51,4 @@ class PostResponse(PostBase):
     id: int
     user_id: int
     date_posted: datetime
-    author: UserResponse
+    author: UserPublic
